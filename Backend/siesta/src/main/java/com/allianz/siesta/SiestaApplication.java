17edd6424dc205f81c9b91b1791afa7c4c617408
yuppry <yuppry@gmail.com>
2020-09-21 12:24:39 +0700

    [Added]: init backend project name as Siesta | [Deleted]: test init backend

---
+++ Backend/siesta/src/main/java/com/allianz/siesta/SiestaApplication.java
@@ -0,0 +1,13 @@
+package com.allianz.siesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiestaApplication.class, args);
+	}
+
+}
