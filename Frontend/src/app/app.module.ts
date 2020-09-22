2a9cb8f503c256a7d549f00936d0c75c8328c602
EKKARATPRO <ekkarat.promrit@allianz.com>
2020-09-22 17:34:57 +0700

    [Edited]: install bootstrap

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardComponent } from './card/card.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { CardFormComponent } from './card-form/card-form.component';
+
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, CardComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NavbarComponent, CardComponent, CardFormComponent,],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule,
+    ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
