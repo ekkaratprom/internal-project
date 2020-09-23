bdce8d1275c928f9f9994930343d3c5fd4dda5d1
EKKARATPRO <ekkarat.promrit@allianz.com>
2020-09-23 10:24:07 +0700

    [Edited]: decare filterpipe

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardComponent } from './card/card.component';
-import { FormsModule } from '@angular/forms';
+
 
 import { HttpClientModule } from '@angular/common/http';
 import { CardFormComponent } from './card-form/card-form.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, CardComponent, CardFormComponent,],
+  declarations: [AppComponent, NavbarComponent, CardComponent, CardFormComponent, FilterPipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule,
     ReactiveFormsModule],
   providers: [],
