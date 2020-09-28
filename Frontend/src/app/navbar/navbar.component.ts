480aeaa6dea60d880d59bb3e2f01e4f4e29f20f9
kerati9702 <kerati9702@gmail.com>
2020-09-28 14:47:31 +0700

    [edited] make date function dynamic

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { DYNAMIC_TYPE } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { parseMessage } from '@angular/localize/src/utils';
 import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -10,20 +11,45 @@ import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 export class NavbarComponent implements OnInit {
 
   DMY = this.calendar.getToday();
+
+
   monthlist = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
+
   month = '';
+  weekdays = [0, 0, 0, 0, 0, 0, 0];
   today = this.calendar.getToday();
+  date = 8;
+
   title = 'angular-text-search-highlight';
 
   constructor(private calendar: NgbCalendar) {
     this.changeMonth()
+    this.changeday()
+    this.weekdayfn()
   }
-  changeMonth() {
+  public changeMonth() {
     this.month = this.monthlist[this.DMY.month - 1];
   }
-  ngOnInit(): void {
+  public changeday() {
+    this.date = this.calendar.getWeekday(this.DMY);
+  }
+  public weekdayfn() {
+    this.weekdays.splice(this.date, 1, this.DMY.day)
+    for (let i = 0; i < 7; i++) {
+      if (i < this.date) {
+        let diff = this.date - i
+        let previousday = this.calendar.getPrev(this.DMY, "d", diff)
+        this.weekdays[i] = previousday.day
+      }
+      if (i > this.date) {
+        let diff = i - this.date
+        let previousday = this.calendar.getNext(this.DMY, "d", diff)
+        this.weekdays[i] = previousday.day
+      }
+    }
 
   }
-
+  ngOnInit(): void {
+  }
 }
