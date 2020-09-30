66be2f14276094d7b36af606654fb60cef6200e1
yuppry <yuppry@gmail.com>
2020-09-30 10:50:38 +0700

    [Added]: API GetAllAssignees for dropdownlist

--- Backend/siesta/src/main/java/com/allianz/siesta/user/service/UserService.java
+++ Backend/siesta/src/main/java/com/allianz/siesta/user/service/UserService.java
@@ -3,10 +3,13 @@ package com.allianz.siesta.user.service;
 
 import com.allianz.siesta.user.User;
 import com.allianz.siesta.user.UserRequest;
+import com.allianz.siesta.user.UserResponse;
 
 public interface UserService {
 
     User saveUser(UserRequest userRequest);
 
     Iterable<User> getAllUsers();
+
+    Iterable<UserResponse> getAllAssignees();
 }
