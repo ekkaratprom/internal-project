22cda478953eed3045f797ba220c5cbadbae407e
yuppry <yuppry@gmail.com>
2020-10-14 14:23:28 +0700

    [Created]: Assignment service + API POST assignment and API GET all.

---
+++ Backend/siesta/src/main/java/com/allianz/siesta/assignment/service/AssignmentServiceImpl.java
@@ -0,0 +1,26 @@
+package com.allianz.siesta.assignment.service;
+
+import com.allianz.siesta.assignment.Assignment;
+import com.allianz.siesta.assignment.AssignmentRepository;
+import com.allianz.siesta.assignment.AssignmentRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AssignmentServiceImpl implements AssignmentService {
+
+    @Autowired
+    private AssignmentRepository assignmentRepository;
+
+    @Override
+    public Assignment addAssignment(AssignmentRequest assignmentRequest) {
+        Assignment assignment = assignmentRequest.assignmentRequest();
+        assignment.setDeletedStatus(false);
+        return assignmentRepository.save(assignment);
+    }
+
+    @Override
+    public Iterable<Assignment> getAllAssignments(){
+        return assignmentRepository.findAll();
+    }
+}
