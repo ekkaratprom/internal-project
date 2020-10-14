f37f56e3b20e10cb2b2240af289d382ddc7611a9
yuppry <yuppry@gmail.com>
2020-10-14 14:20:48 +0700

    [Edited]: Comment code from st sprint.

--- Backend/siesta/src/main/java/com/allianz/siesta/project/ProjectController.java
+++ Backend/siesta/src/main/java/com/allianz/siesta/project/ProjectController.java
@@ -1,25 +1,25 @@
-package com.allianz.siesta.project;
-
-import com.allianz.siesta.project.service.ProjectService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping(value = "/api")
-public class ProjectController {
-
-    @Autowired
-    private ProjectService projectService;
-
-    @CrossOrigin
-    @PostMapping (value = "/v1/addProject")
-    public Project addProject(@RequestBody ProjectRequest projectRequest){
-        return projectService.saveProject(projectRequest);
-    }
-
-    @CrossOrigin
-    @GetMapping (value = "/v1/project")
-    public Iterable<ProjectResponse> getAllProjects(){
-        return projectService.getAllProjects();
-    }
-}
+//package com.allianz.siesta.project;
+//
+//import com.allianz.siesta.project.service.ProjectService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping(value = "/api")
+//public class ProjectController {
+//
+//    @Autowired
+//    private ProjectService projectService;
+//
+//    @CrossOrigin
+//    @PostMapping (value = "/v1/addProject")
+//    public Project addProject(@RequestBody ProjectRequest projectRequest){
+//        return projectService.saveProject(projectRequest);
+//    }
+//
+//    @CrossOrigin
+//    @GetMapping (value = "/v1/project")
+//    public Iterable<ProjectResponse> getAllProjects(){
+//        return projectService.getAllProjects();
+//    }
+//}
--- Backend/siesta/src/main/java/com/allianz/siesta/project/ProjectResponse.java
+++ Backend/siesta/src/main/java/com/allianz/siesta/project/ProjectResponse.java
@@ -1,28 +1,28 @@
-package com.allianz.siesta.project;
-
-public class ProjectResponse {
-    private Long projectId;
-    private String projectName;
-
-    public ProjectResponse(Long projectId, String projectName) {
-        this.projectId = projectId;
-        this.projectName = projectName;
-    }
-
-    public Long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Long projectId) {
-        this.projectId = projectId;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-}
+//package com.allianz.siesta.project;
+//
+//public class ProjectResponse {
+//    private Long projectId;
+//    private String projectName;
+//
+//    public ProjectResponse(Long projectId, String projectName) {
+//        this.projectId = projectId;
+//        this.projectName = projectName;
+//    }
+//
+//    public Long getProjectId() {
+//        return projectId;
+//    }
+//
+//    public void setProjectId(Long projectId) {
+//        this.projectId = projectId;
+//    }
+//
+//    public String getProjectName() {
+//        return projectName;
+//    }
+//
+//    public void setProjectName(String projectName) {
+//        this.projectName = projectName;
+//    }
+//
+//}
