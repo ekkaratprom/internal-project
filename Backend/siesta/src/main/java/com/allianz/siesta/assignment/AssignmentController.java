b9c607357058f38c7e8e599eaf0642cfbb8d6e36
Ekkarat Promrit <ekkarat.prom@gmail.com>
2020-10-15 09:51:42 +0700

    [Edited] solve backend by big

--- Backend/siesta/src/main/java/com/allianz/siesta/assignment/AssignmentController.java
+++ Backend/siesta/src/main/java/com/allianz/siesta/assignment/AssignmentController.java
@@ -2,7 +2,6 @@ package com.allianz.siesta.assignment;
 
 
 import com.allianz.siesta.assignment.service.AssignmentService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
