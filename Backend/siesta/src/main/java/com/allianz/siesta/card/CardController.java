2a65c06f3f2dd7c42e8008cc91a181bac24b9e09
yuppry <yuppry@gmail.com>
2020-10-15 14:45:52 +0700

    [Edited]: add API getAllCards.

---
+++ Backend/siesta/src/main/java/com/allianz/siesta/card/CardController.java
@@ -0,0 +1,22 @@
+package com.allianz.siesta.card;
+
+import com.allianz.siesta.card.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class CardController {
+
+    @Autowired
+    private CardService cardService;
+
+    @CrossOrigin
+    @GetMapping(path = "/v1/card")
+    public Iterable<AssignmentResponse> getAllCards() {
+        return cardService.getAllCards();
+    }
+}
