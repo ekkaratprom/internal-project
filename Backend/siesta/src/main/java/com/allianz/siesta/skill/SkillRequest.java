02aa070418d8bcfe390d99118c94a9b578f003e7
yuppry <yuppry@gmail.com>
2020-10-26 12:36:46 +0700

    [Added]: add request iconPath in SkillRequest

--- Backend/siesta/src/main/java/com/allianz/siesta/skill/SkillRequest.java
+++ Backend/siesta/src/main/java/com/allianz/siesta/skill/SkillRequest.java
@@ -2,9 +2,11 @@ package com.allianz.siesta.skill;
 
 public class SkillRequest {
     private String skillName;
+    private String iconPath;
 
-    public SkillRequest(String skillName) {
+    public SkillRequest(String skillName, String iconPath) {
         this.skillName = skillName;
+        this.iconPath = iconPath;
     }
 
     public String getSkillName() {
@@ -15,9 +17,18 @@ public class SkillRequest {
         this.skillName = skillName;
     }
 
+    public String getIconPath() {
+        return iconPath;
+    }
+
+    public void setIconPath(String iconPath) {
+        this.iconPath = iconPath;
+    }
+
     public Skill skillRequest() {
         Skill skill = new Skill();
         skill.setSkillName(skillName);
+        skill.setIconPath(iconPath);
 
         return skill;
     }
