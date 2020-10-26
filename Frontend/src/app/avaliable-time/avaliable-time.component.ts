fdc510c30db292b3bdce245a61991f911ead264b
kerati9702 <kerati9702@gmail.com>
2020-10-26 14:57:49 +0700

    [edited] merged ekkarat

--- Frontend/src/app/avaliable-time/avaliable-time.component.ts
+++ Frontend/src/app/avaliable-time/avaliable-time.component.ts
@@ -21,6 +21,8 @@ export class AvaliableTimeComponent implements OnInit {
   assignments: AssignmentResponse[] = [];
   searchText = '';
   availibleUsers;
+  positionCheck = null;
+  skillsetCheck = null;
   userDetail;
   result = [];
   completedStatusCheck = undefined;
