d3df82dd3df6895f5952b278525655f8a1ccc9da
yuppry <yuppry@gmail.com>
2020-10-30 10:51:07 +0700

    [Edited]: modify API Post and Get

--- Backend/siesta/src/main/java/com/allianz/siesta/user/UserRequest.java
+++ Backend/siesta/src/main/java/com/allianz/siesta/user/UserRequest.java
@@ -1,50 +1,53 @@
-//package com.allianz.siesta.user;
-//
-//public class UserRequest {
-//    private String firstName;
-//    private String lastName;
-//    private String email;
-//    private String position;
-//
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPosition() {
-//        return position;
-//    }
-//
-//    public void setPosition(String position) {
-//        this.position = position;
-//    }
-//
-//    public User userRequest(){
-//        User user = new User();
-//        user.setFirstName(firstName);
-//        user.setLastName(lastName);
-//        user.setEmail(email);
-//        user.setPosition(position);
-//        return user;
-//    }
-//}
+package com.allianz.siesta.user;
+
+import com.allianz.siesta.position.Position;
+
+public class UserRequest {
+    private Long positionId;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(Long positionId) {
+        this.positionId = positionId;
+    }
+
+    public User userRequest(){
+        User user = new User();
+        user.setEmail(email);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setPosition(new Position(positionId));
+
+        return user;
+    }
+}
--- Backend/siesta/src/main/java/com/allianz/siesta/user/service/UserServiceImpl.java
+++ Backend/siesta/src/main/java/com/allianz/siesta/user/service/UserServiceImpl.java
@@ -1,53 +1,37 @@
-//package com.allianz.siesta.user.service;
-//
-//import com.allianz.siesta.task.Task;
-//import com.allianz.siesta.task.TaskRequest;
-//import com.allianz.siesta.user.User;
-//import com.allianz.siesta.user.UserRepository;
-//import com.allianz.siesta.user.UserRequest;
-//import com.allianz.siesta.card.response.UserResponse;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class UserServiceImpl implements UserService {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Override
-//    public User saveUser(UserRequest userRequest) {
-//        User user = userRequest.userRequest();
-//        return userRepository.save(user);
-//    }
-//
-//    @Override
-//    public Iterable<User> getAllUsers(){
-//        return userRepository.findAll();
-//    }
-//
-//    @Override
-//    public Iterable<UserResponse> getAllAssignees() {
-//        List<UserResponse> userResponseList = new ArrayList<>();
-//        Iterable<User> users = userRepository.findAll();
-//        for (User user : users){
-//            String assignee = user.getFirstName() + " " + user.getLastName();
-//            UserResponse userResponse = new UserResponse(
-//                    user.getId(),
-//                    assignee
-//            );
-//            userResponseList.add(userResponse);
-//        }
-//        return userResponseList;
-//    }
-//
-//    @Override
-//    public Optional<User> findUserById(Long id){
-//        return userRepository.findById(id);
-//    }
-//}
+package com.allianz.siesta.user.service;
+
+import com.allianz.siesta.user.User;
+import com.allianz.siesta.user.UserRepository;
+import com.allianz.siesta.user.UserRequest;
+import com.allianz.siesta.card.response.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User addUser(UserRequest userRequest) {
+        User user = userRequest.userRequest();
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+    @Override
+    public Optional<User> findUserById(Long id){
+        return userRepository.findById(id);
+    }
+}
