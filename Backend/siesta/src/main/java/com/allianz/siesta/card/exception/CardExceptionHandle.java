6b0c38e15a780c1b2dc64b877701178e42e9c390
yuppry <yuppry@gmail.com>
2020-11-19 12:35:17 +0700

    [Edited]: refactor CardExceptionHandle name

--- Backend/siesta/src/main/java/com/allianz/siesta/card/exception/CardExceptionHandle.java
+++ Backend/siesta/src/main/java/com/allianz/siesta/card/exception/CardExceptionHandle.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class CardNotFoundExceptionHandle {
+public class CardExceptionHandle {
     @ResponseBody
     @ExceptionHandler({CardNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
